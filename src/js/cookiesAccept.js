38333dff288302d6e68d3a35016ce0c963b85bc6
SirotovAlexander <alexander.sirotov@gmail.com>
2024-04-10 23:48:08 +0300

    done cokies
    done cokies

--- src/js/cookiesAccept.js
+++ src/js/cookiesAccept.js
@@ -3,3 +3,15 @@ const cookiesButtonAccept = document.querySelector('.cookies__button');
 
 console.dir(cookiesInfoBox);
 console.dir(cookiesButtonAccept);
+
+function showAccept() {
+  cookiesInfoBox.classList.remove('visually-hidden');
+}
+
+setTimeout(showAccept, 2000);
+
+cookiesButtonAccept.addEventListener('click', onClick);
+
+function onClick(e) {
+  cookiesInfoBox.classList.add('visually-hidden');
+}
