e54e99923a5233f755c880f2bf323a66a3bef308
SirotovAlexander <alexander.sirotov@gmail.com>
2024-04-12 23:21:05 +0300

    done modal
    done modal

--- src/js/modalvalidate.js
+++ src/js/modalvalidate.js
@@ -1,27 +1,30 @@
-const modalWindow = document.querySelector('.bacdrop');
+import IMask from 'imask';
+
+const bacdrop = document.querySelector('.bacdrop');
+const modalWindow = document.querySelector('.modalWindov');
+const thanksWindow = document.querySelector('.thanksWrapper');
 const modalForm = document.querySelector('.modal-form');
 const nameArea = document.getElementById('name-area');
 const telArea = document.getElementById('tel-area');
-const submiteButton = document.querySelector('.modal__submit');
 
 modalForm.addEventListener('submit', onSubmit);
-console.dir(nameArea);
-console.dir(telArea);
+
+IMask(telArea, {
+  mask: '+380-00-000-00-00',
+});
 
 function onSubmit(e) {
   e.preventDefault();
-  if (nameArea.value === '' && telArea.value === '') {
-    nameArea.classList.add('checkeError');
-    telArea.classList.add('checkeError');
-  } else if (nameArea.value === '' && telArea.value !== '') {
-    nameArea.classList.add('checkeError');
-  } else if (nameArea.value !== '' && telArea.value === '') {
-    telArea.classList.add('checkeError');
-  } else {
-    nameArea.value = '';
-    telArea.value = '';
-    telArea.classList.remove('checkeError');
-    nameArea.classList.remove('checkeError');
-    modalWindow.classList.add('visually-hidden');
-  }
+  nameArea.value = '';
+  telArea.value = '';
+  telArea.classList.remove('checkeError');
+  nameArea.classList.remove('checkeError');
+  modalWindow.classList.add('visually-hidden');
+  thanksWindow.classList.remove('visually-hidden');
+  setTimeout(close, 2000);
+}
+function close() {
+  bacdrop.classList.add('visually-hidden');
+  thanksWindow.classList.add('visually-hidden');
+  modalWindow.classList.remove('visually-hidden');
 }
